f2863f54253d6605d927899f954c10c88e473d4c
David McGrann <dpmcgrann@gmail.com>
2018-07-12 00:29:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
       LIMIT 1
       SQL
 
-      DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.flatten
+      DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
 
   def save
